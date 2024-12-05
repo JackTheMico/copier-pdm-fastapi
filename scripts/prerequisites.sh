59c13543fa3d645c8498f8128b68c33b0ba6ab96
jackwenyoung <dlwxxxdlw@gmail.com>
2024-12-05 14:18:08 +0800

    add containerEnv

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Makefile Install PDM"
+echo "Prequisites: Install PDM and Pyenv"
 
 if ! command -v pdm &>/dev/null; then
   if ! command -v pipx &>/dev/null; then
     python3 -m pip install --user pipx
   fi
-  pipx install pdm
+  pipx install pdm pyenv
+fi
+
+echo "Prequisites: Pyenv Install Python Versions"
+
+if [ -n "${PDM_MULTIRUN_VERSIONS}" ]; then
+  if [ "${PDM_MULTIRUN_USE_VENVS}" -eq "1" ]; then
+    for version in ${PDM_MULTIRUN_VERSIONS}; do
+      pyenv install "${version}"
+    done
+  fi
+  pdm multirun -v pdm install -G:all
 fi
