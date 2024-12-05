445d9dfe2e71f9520e2efdb1812c8dffb11f87a1
jackwenyoung <dlwxxxdlw@gmail.com>
2024-12-05 13:51:36 +0800

    add debug info

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Makefile Install PDM"
+
 if ! command -v pdm &>/dev/null; then
   if ! command -v pipx &>/dev/null; then
     python3 -m pip install --user pipx
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Makefile PDM Install Environments And Dependencies"
+
 if ! command -v pdm &>/dev/null; then
   if ! command -v pipx &>/dev/null; then
     python3 -m pip install --user pipx
@@ -10,6 +12,9 @@ fi
 if ! pdm self list 2>/dev/null | grep -q pdm-multirun; then
   pdm install --plugins
 fi
+
+echo "PDM_MULTIRUN_VERSIONS=${PDM_MULTIRUN_VERSIONS}"
+echo "PDM_MULTIRUN_USE_VENVS=${PDM_MULTIRUN_USE_VENVS}"
 
 if [ -n "${PDM_MULTIRUN_VERSIONS}" ]; then
   if [ "${PDM_MULTIRUN_USE_VENVS}" -eq "1" ]; then
